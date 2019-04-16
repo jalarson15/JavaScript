f34a204a25bdc4f354dfb2a90bad7582493185e4
jalarson15 <46009488+jalarson15@users.noreply.github.com>
2019-04-16 14:29:07 -0700

    Add files via upload
    variable assignment, writing to console, simple mathematics

---
+++ larson_james_lab1.js
@@ -0,0 +1,18 @@
+//problem 1
+let aStr = -5;
+let aNumber;
+aNumber = parseInt(aStr);
+posNum = Math.abs(aNumber);
+console.log(posNum);
+
+//problem 2
+var farenheit = 212;
+let celsius = 5 / 9 * (farenheit - 32);
+console.log("the temperature in celsius is " + celsius);
+
+//problem 3
+let radius = 5;
+let length = 10;
+let area = Math.PI * Math.pow(radius, 2);
+let volume = area * length;
+console.log("The cylinder area is " + area.toFixed(2) + " and the volume is " + volume.toFixed(2));
