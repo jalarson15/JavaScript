580e480e1c02adca7c15ccb5c67994a63da75a67
jalarson15 <46009488+jalarson15@users.noreply.github.com>
2019-04-16 14:30:48 -0700

    Update larson_james_lab1.js

--- larson_james_lab1.js
+++ larson_james_lab1.js
@@ -1,4 +1,5 @@
 //problem 1
+//absolute value of a string
 let aStr = -5;
 let aNumber;
 aNumber = parseInt(aStr);
@@ -6,11 +7,13 @@ posNum = Math.abs(aNumber);
 console.log(posNum);
 
 //problem 2
+//converting farenheit to celsius
 var farenheit = 212;
 let celsius = 5 / 9 * (farenheit - 32);
 console.log("the temperature in celsius is " + celsius);
 
 //problem 3
+//finding rhe area and volume of a given cylinder
 let radius = 5;
 let length = 10;
 let area = Math.PI * Math.pow(radius, 2);
